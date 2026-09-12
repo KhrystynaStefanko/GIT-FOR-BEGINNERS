1f092f1ee30f8484c3b96f6b88f529fae338544b
Khrystyna Stefanko <chrystynastefanko1994@gmail.com>
2026-09-12 21:58:22 +0300

    add descriptive labels on main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from utils.calculator import add_nums, divide_nums, subtract_nums, multiply_nums, power
 
 if __name__ == "__main__":
-    print(add_nums(1,3))
-    print(divide_nums(6,3))
-    print(subtract_nums(6,3))
-    print(multiply_nums((2,4)))
-    print(power((3,4)))
+    print("Addition:", add_nums(1,3))
+    print("Division:", divide_nums(6,3))
+    print("Subtraction:", subtract_nums(6,3))
+    print("Multiplication:", multiply_nums((2,4)))
+    print("Power:", power((3,4)))
