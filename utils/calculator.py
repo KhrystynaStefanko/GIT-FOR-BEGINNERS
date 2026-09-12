e059ebb6281c7e1bfb8bea9ab3409d6f5853ce77
Khrystyna Stefanko <chrystynastefanko1994@gmail.com>
2026-09-12 23:26:52 +0300

    added docstring to calculator.py

--- utils/calculator.py
+++ utils/calculator.py
@@ -1,14 +1,19 @@
 def add_nums (a,b):
+    """Returns the sum of a and b"""
     return a + b
 
 def divide_nums(a,b):
-        return a / b
+    """Returns the devision of a and b"""
+    return a / b
 
 def subtract_nums(a,b):
+    """Returns the difference of a and b"""
     return a - b
 
-def multiply_nums(a,b)
+def multiply_nums(a,b):
+    """Returns the product of a of b"""
     return a * b
 
 def power(a,b):
+    """Returns a raised to the power of b"""
     return a ** b
