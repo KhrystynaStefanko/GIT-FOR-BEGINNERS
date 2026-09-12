bc3268b77d048583f05a7a386a767503f43a6cad
Khrystyna Stefanko <chrystynastefanko1994@gmail.com>
2026-09-12 18:43:00 +0300

    updated main file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from utils.calculator import add_nums, divide_nums, subtract_nums, multiply_nums
+from utils.calculator import add_nums, divide_nums, subtract_nums, multiply_nums, power
 
 if __name__ == "__main__":
     print(add_nums(1,3))
     print(divide_nums(6,3))
     print(subtract_nums(6,3))
     print(multiply_nums((2,4)))
+    print(power((3,4)))
