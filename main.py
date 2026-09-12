4236f2b4810088d89552590fbbf0cabad2ff1abe
Khrystyna Stefanko <chrystynastefanko1994@gmail.com>
2026-09-12 18:19:44 +0300

    added multiply_nums function and updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from utils.calculator import add_nums, divide_nums, subtract_nums, power
+from utils.calculator import add_nums, divide_nums, subtract_nums
 
 if __name__ == "__main__":
     print(add_nums(1,3))
     print(divide_nums(6,3))
     print(subtract_nums(6,3))
-    print(power(2,4))
--- utils/calculator.py
+++ utils/calculator.py
@@ -7,5 +7,8 @@ def divide_nums(a,b):
 def subtract_nums(a,b):
     return a-b
 
+def multiply_nums(a,b)
+    return a*b
+    
 def power(a,b):
     return a**b
