420ee4bbd9a5205da5aefe8f716dfb111db567b8
Khrystyna Stefanko <chrystynastefanko1994@gmail.com>
2026-09-13 20:52:54 +0300

    updated main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     print("Division:", divide_nums(6,3))
     print("Subtraction:", subtract_nums(6,3))
     print("Multiplication:", multiply_nums((2,4)))
-    print("Power:", power((3,4)))
+    print("Power:", power((9,4)))
